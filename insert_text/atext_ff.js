3f9d43b425db69f7dd7efd488144854e235a07be
oPen syLar <open.ml@hotmail.com>
2018-04-05 01:02:24 -0400

    stable

--- insert_text/atext_ff.js
+++ insert_text/atext_ff.js
@@ -1,35 +1,206 @@
-$( document ).ready(function() {
+const PASTECLIPBOARD = '{kp : {"clpbrd": 2}}';
 
 
 
-function InitWs(payload)
+
+function SmPostClick()
 {
 
 
 
-    var wsUri = "ws://2.2.2.2:5463";
+    //$('input[name="title"]').focus();
+    //$('input[name="title"]').select();
+    //document.execCommand("copy");
+    //$('.button').removeAttr("disabled");
+
+
+
+    return 0;   
+
+
+
+}
+
+
+
+function SmFormatDescription()
+{
+    return 0;
+}
+
+
+
+
+function SmPostCategory(categorys)
+{
+
+    inputNameCategory = 'input[name="category"]';
+
+
+    $(inputNameCategory).val(categorys);
+    $().focus();
+    $(inputNameCategory).select();
+    document.execCommand("copy");
+    $(inputNameCategory).val("");
+
+    return 0;
+
+
+}
+
+
+
+
+function SmPostTitle(title)
+{
+
+
+    $('input[name="title"]').val(title);
+
+    return 0;
+
+
+}
+
+
+
+
+function SmPostDescription(description)
+{
+
+
+
+    $('textarea[name="body"]').val(description);
+
+    return 0;
+
+
+}
+
+
+
+
+function RestfulGetPost()
+{
+
+
+    $.ajax(
+    {
+
+        url: "http://localh0t/webapi-ostroker/api.php/data"
+    }
+
+
+
+
+
+    ).then(function(data)
+    {
+
+        //alert("Its done");
+
+
+        $.each(data["data"]["records"], function(key, value)
+        {
+
+
+
+            artTitle = value[1];
+            artLink = value[2];
+            artDescription = value[3];
+            tags = null;
+            disclaimerFooter = null;
+            disclaimerHeader = null;
+            imgSeparator = null;
+            formatDescription = null;
+
+
+
+
+
+            //FormatDescription(formatDescription, imgSeparator, disclaimerHeader, disclaimerFooter, tags);
+
+
+
+
+
+            //PagePostSm(artTitle, artDescription, tags);
+
+
+
+
+            //console.log(artDescription);
+          
+
+
+        });
+
+
+
+    }
+
+
+
+    ).fail(function()
+    {
+
+
+        alert("[!] Error Restful connection");
+
+
+    });
+
+
+
+
+    return 0;
+}
+
+
+
+function WebSocketoStroker(payload)
+{
+
+
+
+
+
+    var wsUri = "ws://2.2.2.252:5463";
     var websocket;
+
+
 
 
     try {
 
 
         if (typeof MozWebSocket == 'function')
+        {
             WebSocket = MozWebSocket;
+        }
+            
+
 
 
         if ( websocket && websocket.readyState == 1 )
+        {
             websocket.close();
+        }
+            
+
 
 
 
         websocket = new WebSocket(wsUri);
 
 
+
+
         websocket.onopen = function (evt)
         {
 
             websocket.send(payload);
+            //websocket.close();
             
         };
 
@@ -61,55 +232,27 @@ function InitWs(payload)
 
     catch (exception)
     {
-        //alert('[!] ERROR: CONNECTING');
+        alert('[!] ERROR: CONNECTING ' + exception);
     }
+
+
 
 }
 
 
+$( document ).ready(function() {
 
 
-//var strPayload = '{ "kp" : { "txt":"oPen syLar" , "dly":10 }}'; 
+    //RestfulGetPost();
 
 
-
-keyS = "Fucking ass =)";
-
+    WebSocketoStroker(PASTECLIPBOARD);
 
 
-var strRaw = '{"kp" : { "txt": "' + keyS + '", "dly": 2 }}';
-
-
-//var strPayload = JSON.stringify(strRaw);
-
-
-
-InitWs(strRaw);
-
-
-
-//$('input[name="password"]').val("pepeeee");
-
-
-
-$('.button').each(function(i, obj) {
+    //SmPostCategory("linux gdb codigo abierto");
+    //SmPostTitle("Que es esooo");
+    //SmPostDescription("Ministerio");
+    //SmPostClick();
     
-
-    //alert($(obj).text());
-
-
-	if($(obj).text()=="Login")
-	{
-		//$(obj).removeAttr("disabled");
-		$(obj).click();
-		alert("click press");
-
-	}
-
-
-
-});
-
-
 
 });
